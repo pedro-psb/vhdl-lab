eaead004088d02558906270c785739041d703d79
Pedro Brochado <pedropsb95@gmail.com>
2025-09-19 10:52:53 -0300

    Update exercise 3 example to use multi-arch

--- exercises/03-function-f/design.vhd
+++ exercises/03-function-f/design.vhd
@@ -1,9 +1,9 @@
 -- Funcao booleana F
+
 library ieee;
 use ieee.std_logic_1164.all;
 
 
--- Attribuicao simultanea
 entity fn_f is
   port(
     a,b,c,d : in std_logic;
@@ -12,12 +12,45 @@ entity fn_f is
 end entity;
 
 
-architecture behavior of fn_f is
+-- atribuicao simultanea
+architecture simultanea of fn_f is
 begin
   o <= (
-    ((not a) and (c) and (not d))
+    ((not a) and c and (not d))
     or ((not b) and c)
     or (b and c and (not d))
   );
+end architecture;
+
+
+-- atribuicao conditional
+architecture condicional of fn_f is
+begin
+  o <=
+    '1' when (a='0' and c='1' and d='0') else
+    '1' when (b='0' and c='1') else
+    '1' when (b='1' and c='1' and d='0') else
+    '0';
+end architecture;
+
+
+-- atribuicao selecionada
+architecture selecionada of fn_f
+is
+	signal pack : std_logic_vector(3 downto 0);
+begin
+	pack <= a & b & c & d;
+  with pack select
+      o <=
+        -- '1' when "0-10",
+        '1' when "0010",
+        '1' when "0110",
+        -- '1' when "-01-",
+        '1' when "0011",
+        '1' when "1010",
+        '1' when "1011",
+        -- '1' when "-110",
+        '1' when "1110",
+        '0' when others;
 end architecture;
 
--- exercises/03-function-f/testbench.vhd
+++ exercises/03-function-f/testbench.vhd
@@ -6,18 +6,36 @@ entity tb_fn_f is
 end entity;
 
 
-architecture sim of tb_fn_f is
+architecture sim of tb_fn_f
+is
   signal a,b,c,d,o : std_logic;
+  signal pack : std_logic_vector(3 downto 0);
 begin
-  uut : entity work.fn_f port map(
+  uut : entity work.fn_f(selecionada) port map(
     a => a, b => b, c => c, d => d, o => o
   );
+  a <= pack(0);
+  b <= pack(1);
+  c <= pack(2);
+  d <= pack(3);
   process
   begin
-      a <= '0'; b <= '0'; c <= '0'; d <= '0'; wait for 10 ns;
-      a <= '0'; b <= '0'; c <= '0'; d <= '1'; wait for 10 ns;
-      a <= '0'; b <= '0'; c <= '1'; d <= '0'; wait for 10 ns;
-      a <= '0'; b <= '0'; c <= '1'; d <= '1'; wait for 10 ns;
-      wait;
+    pack <= "0000"; wait for 10 ns;
+    pack <= "0001"; wait for 10 ns;
+    pack <= "0010"; wait for 10 ns;
+    pack <= "0011"; wait for 10 ns;
+    pack <= "0100"; wait for 10 ns;
+    pack <= "0101"; wait for 10 ns;
+    pack <= "0110"; wait for 10 ns;
+    pack <= "0111"; wait for 10 ns;
+    pack <= "1000"; wait for 10 ns;
+    pack <= "1001"; wait for 10 ns;
+    pack <= "1010"; wait for 10 ns;
+    pack <= "1011"; wait for 10 ns;
+    pack <= "1100"; wait for 10 ns;
+    pack <= "1101"; wait for 10 ns;
+    pack <= "1110"; wait for 10 ns;
+    pack <= "1111"; wait for 10 ns;
+    wait;
   end process;
 end architecture;
